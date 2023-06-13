3128d39f524321b0f42da829af08e992fdaf776c
Metarails <samisantamaa87@gmail.com>
2023-06-13 10:10:11 +0300

    part c loppu

--- Osa1/part1/src/App.js
+++ Osa1/part1/src/App.js
@@ -1,14 +1,23 @@
-const Hello = (props) => {
-  console.log(props)
+import { useState } from 'react'
 
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+// const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  // console.log(props)
+
+  // const { name, age } = props
+  // const name = props.name;
+  // const age = props.age;
+
+  // const bornYear = () => {
+  //   const yearNow = new Date().getFullYear()
+  //   return yearNow - props.age
+  // }
+  const bornYear = () => new Date().getFullYear() - age
+
 
   return (
     <div>
-      <p>Hello prop {props.name}, you are {props.age} years old</p>
+      <p>Hello prop {name}, you are {age} years old</p>
       <p>
         So you wre probably born {bornYear()}
       </p>
@@ -16,7 +25,33 @@ const Hello = (props) => {
   )
 }
 
-const App = () => {
+const App = (props) => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  // const handleClick = () => {
+  //   console.log("clicked")
+  // }
+  // setTimeout(
+  //   () => setCounter(counter + 1), 1000
+  // )
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  }
+
+  console.log('rendering...', counter)
+
+  // const { counter } = props
   const now = new Date();
   const a = 10;
   const b = 20;
@@ -35,7 +70,17 @@ const App = () => {
   console.log(friends[1])
 
   return (
-    <>
+    <div>
+      <Display counter={counter} />
+      {/* <p>Lasketaan: {counter}</p> */}
+      {/* <button onClick={increaseByOne}> */}
+      {/* <button onClick={() => setCounter(counter + 1)}> */}
+      {/* plussaus</button>
+      <button onClick={setToZero}>ZERO</button> */}
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={decreaseByOne} text="minus" />
+      <Button handleClick={setToZero} text="zero" />
+
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={nimi} age={ika} />
@@ -51,8 +96,29 @@ const App = () => {
       <p>
         printing: {friends2}
       </p>
-    </>
+    </div>
   )
 }
+
+// const Display = ({ counter }) => {
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+// const Button = (props) => {
+//   return (
+//     <button onClick={props.handleClick}>
+//       {props.text}
+//     </button>
+//   )
+// }
 
 export default App;
--- Osa1/part1/src/index.js
+++ Osa1/part1/src/index.js
@@ -3,4 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+// let counter = 1
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+// const refresh = () => {
+//     root.render(<App counter={counter} />);
+// }
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+// }, 1000)
