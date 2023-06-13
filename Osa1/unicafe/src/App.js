304271b97ea1d3c03ba9db38ccab68f0e787e0f6
Metarails <samisantamaa87@gmail.com>
2023-06-13 15:04:38 +0300

    unicafe alku

---
+++ Osa1/unicafe/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      code here
+    </div>
+  );
+}
+
+export default App;
