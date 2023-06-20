16060dbbb20e6799308cc6808f29639564356cab
Metarails <SamiSantamaa87@gmail.com>
2023-06-20 15:07:11 +0300

    2.11 puhelinluettelop step6 DONE

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
+import axios from "axios"
 
 const PersonList = ({persons, filterEntry}) => {
 
@@ -73,15 +74,26 @@ const Filter = ({filterEntry,handleFilterChange}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1231244" },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  // const [persons, setPersons] = useState([
+  //   { name: "Arto Hellas", number: "040-1231244" },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
+  //   { name: 'Dan Abramov', number: '12-43-234345' },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  // ]) 
+
+  const [persons, setPersons] = useState([])
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterEntry, setFilterEntry] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
