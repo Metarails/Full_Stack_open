a6b4f81893ac702fd03d8bb6a7d91f360e1a9858
Metarails <SamiSantamaa87@gmail.com>
2023-06-20 14:43:28 +0300

    osa2 partc opetus

--- Osa2/part1/src/index.js
+++ Osa2/part1/src/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
+// import axios from "axios";
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data;
+//     console.log("Responssi: ", response);
+//     console.log("Nootit: ", notes);
+//     ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+//   })
 
-const result = notes.map(note => note.id)
-console.log("mappaus", result)
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />);
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
+
+// const result = notes.map(note => note.id)
+// console.log("mappaus", result)
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
