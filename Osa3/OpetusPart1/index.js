bbd13269df3c00d47c7a7ae6e8eb4da2b480e5c5
Metarails <SamiSantamaa87@gmail.com>
2023-07-04 12:40:13 +0300

    updating opetus part osa3

--- Osa3/OpetusPart1/index.js
+++ Osa3/OpetusPart1/index.js
@@ -1,7 +1,10 @@
-
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const cors = require("cors")
+
+// const Note = mongoose.model('Note', noteSchema)
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
@@ -44,7 +47,10 @@ app.get("/",(req, res) => {
 })
 
 app.get("/api/notes", (req,res) => {
-    res.json(notes);
+    Note.find({}).then(notes => {
+        res.json(notes)
+      })
+    // res.json(notes);
 })
 
 const generateId = () => {
@@ -60,37 +66,48 @@ app.post("/api/notes", (request, response) => {
     
     const body = request.body;
 
-    if (!body.content){
+    if (body.content === undefined){
         return response.status(400).json({
             error: "content missing"
         });
     }
-    const note = {
+
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
-    // note.id = maxId + 1;
+    })
     
-    console.log(note);
-    notes = notes.concat(note);
-    response.json(note);
+    note.save().then(savedNote =>{
+        response.json(savedNote)
+    })
+    // const note = {
+    //     content: body.content,
+    //     important: body.important || false,
+    //     id: generateId(),
+    // }
+    // note.id = maxId + 1;
+    // console.log(note);
+    // notes = notes.concat(note);
+    // response.json(note);
 })
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    console.log(id);
-    const note = notes.find(note => {
-        const id = Number(request.params.id);
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id);
-        return note.id === id;
-    });
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+      })
+    // const id = request.params.id;
+    // console.log(id);
+    // const note = notes.find(note => {
+    //     const id = Number(request.params.id);
+    //     console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    //     return note.id === id;
+    // });
 
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    // if (note) {
+    //     response.json(note);
+    // } else {
+    //     response.status(404).end();
+    // }
 
     // console.log(note);
     // response.json(note);
