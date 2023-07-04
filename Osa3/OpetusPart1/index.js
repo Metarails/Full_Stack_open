68b59b5826a39924e91ed8da3ea0f982895d8e50
Metarails <SamiSantamaa87@gmail.com>
2023-07-04 12:44:58 +0300

    commenting out

--- Osa3/OpetusPart1/index.js
+++ Osa3/OpetusPart1/index.js
@@ -19,23 +19,23 @@ app.use(express.static("build"));
 app.use(requestLogger);
 app.use(cors());
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: "Browser can execute only JavaScript",
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         important: true
+//     }
+// ]
 
 // const app = http.createServer((request, response) => {
 //     response.writeHead(200, { "Content-Type": "text/json"});
