8612710ed023c771c7e8ce93e561a45571d66d78
Sami Santamaa <121087593+Metarails@users.noreply.github.com>
2023-07-12 14:41:58 +0300

    osa4 opetus part1

--- Osa3/OpetusPart1/index.js
+++ Osa3/OpetusPart1/index.js
@@ -3,201 +3,34 @@ const config = require("./utils/config")
 const logger = require("./utils/logger")
 
 
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require("cors")
+// require("dotenv").config()
+// const express = require("express")
+// const app = express()
+// const cors = require("cors")
 
 // const Note = mongoose.model('Note', noteSchema)
-const Note = require("./models/note")
+// const Note = require("./models/note")
 
-const requestLogger = (request, response, next) => {
-	console.log("Method:", request.method)
-	console.log("Path:  ", request.path)
-	console.log("Body:  ", request.body)
-	console.log("---")
-	next()
-}
+// app.use(express.json())
+// app.use(express.static("build"))
+// app.use(requestLogger)
+// app.use(cors())
 
-const errorHandler = (error, request, response, next) => {
-	console.error("error handler middleware: ", error.message)
-
-	if (error.name === "CastError") {
-		console.log("Cast error log print: ", error.message)
-		return response.status(400).send({ error: "malformatted id" })
-	} else if (error.name === "ValidationError"){
-		console.log("validation error log print: ", error.message)
-		return response.status(400).json({ error: error.message })
-	}
-
-	next(error)
-}
-
-app.use(express.json())
-app.use(express.static("build"))
-app.use(requestLogger)
-app.use(cors())
-
-
-
-
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only JavaScript",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         important: true
-//     }
-// ]
-
-// const app = http.createServer((request, response) => {
-//     response.writeHead(200, { "Content-Type": "text/json"});
-//     response.end(JSON.stringify(notes));
+// app.get("/",(req, res) => {
+// 	res.send("<h1>Hello Wordl</h1>")
 // })
 
-app.get("/",(req, res) => {
-	res.send("<h1>Hello Wordl</h1>")
-})
-
-app.get("/api/notes", (req,res) => {
-	Note.find({}).then(notes => {
-		res.json(notes)
-	})
-	// res.json(notes);
-})
-
-// const generateId = () => {
-// 	const maxId = notes.length > 0
-// 		? Math.max(...notes.map(n => n.id))
-// 		: 0
-// 	return maxId + 1
-// }
-
-
-app.post("/api/notes", (request, response, next) => {
-	console.log("headers: ", request.headers)
-
-	const body = request.body
-
-	// if (body.content === undefined){
-	//     return response.status(400).json({
-	//         error: "content missing"
-	//     });
-	// }
-
-	const note = new Note({
-		content: body.content,
-		important: body.important || false,
-	})
-
-	console.log("posting new note:  ", note)
-
-	note.save()
-		.then(savedNote => {
-			response.json(savedNote)
-		})
-		.catch(error => next(error))
-	// const note = {
-	//     content: body.content,
-	//     important: body.important || false,
-	//     id: generateId(),
-	// }
-	// note.id = maxId + 1;
-	// console.log(note);
-	// notes = notes.concat(note);
-	// response.json(note);
-})
-
-app.get("/api/notes/:id", (request, response, next) => {
-	Note.findById(request.params.id)
-		.then(note => {
-			console.log("what is found note?: ", note)
-			if (note){
-				response.json(note)
-			} else {
-				response.status(404).end()
-			}
-		})
-		.catch(error => {
-			// console.log("error: ", error)
-			console.log("should be going to error middleware next?: ", error.message)
-			next(error)})
-
-	// .catch(error => {
-	//     return next(error)
-	// })
-	//     console.log(error);
-	//     response.status(400).send({error: "malformatted id"});
-	// const id = request.params.id;
-	// console.log(id);
-	// const note = notes.find(note => {
-	//     const id = Number(request.params.id);
-	//     console.log(note.id, typeof note.id, id, typeof id, note.id === id);
-	//     return note.id === id;
-	// });
-
-	// if (note) {
-	//     response.json(note);
-	// } else {
-	//     response.status(404).end();
-	// }
-
-	// console.log(note);
-	// response.json(note);
-})
-
-app.put("/api/notes/:id", (request, response, next) => {
-	// const body = request.body
-	const { content, important } = request.body
-
-	// const note = {
-	//   content: body.content,
-	//   important: body.important,
-	// }
-
-	Note.findByIdAndUpdate(
-		request.params.id, { content ,important },
-		{ new: true , runValidators: true , context: "query" }
-	)
-		.then(updatedNote => {
-			response.json(updatedNote)
-		})
-		.catch(error => next(error))
-})
-
-app.delete("/api/notes/:id", (request, response, next) => {
-
-	Note.findByIdAndRemove(request.params.id)
-		.then(result => {
-			response.status(204).end()
-		})
-		.catch(error => next(error))
-	// const id = Number(request.params.id);
-	// notes = notes.filter(note => note.id !== id);
-
-	// response.status(204).end();
-})
-
-const PORT = process.env.PORT || 3001
+// const PORT = process.env.PORT || 3001
 
 app.listen(config.PORT, () => {
-	logger.log(`Server runnin on port ${config.PORT}`)
+	logger.info(`Server runnin on port ${config.PORT}`)
 })
 
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint" })
-}
+// logger.info(`Server runnin on port ${config.PORT}`)
 
-app.use(unknownEndpoint)
+
+
+// app.use(unknownEndpoint)
 
 // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
-app.use(errorHandler)
+// app.use(errorHandler)
--- Osa3/OpetusPart1/models/note.js
+++ Osa3/OpetusPart1/models/note.js
@@ -1,27 +1,4 @@
 const mongoose = require("mongoose")
-// require('dotenv').config()
-
-// set "REACT_APP_API_KEY=abcdef3242342" && npm start
-// set "REACT_APP_DB_PASSWORD=dddddd " && npm star
-// const password = process.env.REACT_APP_DB_PASSWORD
-// console.log("passwrod: ", password)
-// const url = `mongodb+srv://fullstack:${fullstack@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
-// const url =  `mongodb+srv://Metarail:${password}@metarail0.5iydssj.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-const url = process.env.MONGODB_URI
-
-console.log("connecting to", url)
-
-mongoose.set("strictQuery",false)
-
-mongoose.connect(url)
-	.then(result => {
-		console.log("connected to MongoDB")
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB:", error.message)
-	})
-
 
 const noteSchema = new mongoose.Schema({
 
